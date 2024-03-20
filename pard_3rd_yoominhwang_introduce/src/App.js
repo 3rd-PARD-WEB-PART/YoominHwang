ba2ae86c690f7ceb6de788652dd5d74de336352a
hwangyoomin <yoominh3@gmail.com>
2024-03-20 11:07:40 +0900

    test

--- pard_3rd_yoominhwang_introduce/src/App.js
+++ pard_3rd_yoominhwang_introduce/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { LuInstagram } from "react-icons/lu";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React by PARD
-        </a>
+        <img src="/myphoto.jpg" className="my-photo" alt="Photo" />
+        <h1>
+          황유민
+        </h1>
       </header>
+      <div className="App-bottom">
+      <a
+        className="App-link"
+        href="https://www.instagram.com/yooominh/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <LuInstagram />
+        <br/>
+        My Instagram
+      </a>
+      </div>
     </div>
   );
 }
